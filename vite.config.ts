96ae3caa974a011bed7f4752d874e7113375e6f1
Valencio A. <valencio.araujo@dbtez.com>
2025-10-24 10:23:35 +0530

    Allow Hosts

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts: ["com.theholyredeemer.dbtez.net"], 
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
